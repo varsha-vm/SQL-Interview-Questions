47f958571911e6701daa2b51d0518dd578af467f
Varsha Vaman Murthy <varshavamanmurthy636@gmail.com>
2025-12-08 09:59:15 -0800

    Adding solution to LC 197

---
+++ joins/LC_197_rising_temperature.sql
@@ -0,0 +1,9 @@
+--Write your MySQL query statement below
+--Time: O(N^2)
+--Space: O(N)
+
+SELECT 
+       w1.id
+FROM weather w1 JOIN weather w2 
+ON w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
+WHERE w1.temperature > w2.temperature
