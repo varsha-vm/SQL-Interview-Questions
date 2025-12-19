be635695ce0d73ef7c320f95f4bda491ba094cb4
Varsha Vaman Murthy <varshavamanmurthy636@gmail.com>
2025-12-19 15:13:57 -0800

    adding LC 180 solution

---
+++ joins/LC_180_consecutive_numbers.sql
@@ -0,0 +1,8 @@
+-- Write your MySQL query statement below
+-- Time: O(N+M) => Shuffling due to joins
+-- Space: O(1)
+SELECT 
+        c.num AS ConsecutiveNums
+FROM logs a JOIN logs b ON (a.id = b.id+1) AND (a.num = b.num)
+JOIN logs c on (b.id = c.id+1) AND (b.num = c.num)
+GROUP BY c.num
