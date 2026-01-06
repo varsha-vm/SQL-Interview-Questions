d0542004edf075a4a3272d5acb73093fde6e948c
Varsha Vaman Murthy <varshavamanmurthy636@gmail.com>
2026-01-06 06:55:15 -0800

    Adding solution to 534  problem

---
+++ windows_functions/LC_534_Game Play Analysis III self join.sql
@@ -0,0 +1,11 @@
+-- Time: O(n log n)
+-- Space: O(n)
+SELECT
+        a.player_id,
+        a.event_date,
+        sum(b.games_played) AS games_played_so_far
+FROM activity a JOIN activity b 
+ON a.player_id = b.player_id AND 
+b.event_date <= a.event_date
+GROUP BY a.player_id, a.event_date
+ORDER BY a.player_id, a.event_date 
