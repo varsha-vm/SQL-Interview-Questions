16463fa51c9fb8be01f81695411dc4e6b842c532
Varsha Vaman Murthy <varshavamanmurthy636@gmail.com>
2026-02-12 06:41:54 -0800

    adding solution to seller with no sales

---
+++ subqueries/Sellers With No Sales.sql
@@ -0,0 +1,10 @@
+-- Write your query below
+SELECT
+        seller_name 
+FROM seller
+WHERE seller_id NOT IN (
+(SELECT
+        DISTINCT seller_id
+FROM ORDERS 
+WHERE EXTRACT(YEAR FROM sale_date) = '2020'))
+ORDER BY seller_name
