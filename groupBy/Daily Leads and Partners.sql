531defcebab12e5eed96e6163fa5148bccb12f32
Varsha Vaman Murthy <varshavamanmurthy636@gmail.com>
2026-02-24 06:50:48 -0800

    adding solution to daily leads & partners

---
+++ groupBy/Daily Leads and Partners.sql
@@ -0,0 +1,8 @@
+-- Write your query below
+SELECT
+        date_id,
+        make_name,
+        COUNT(DISTINCT lead_id) AS unique_leads,
+        COUNT(DISTINCT partner_id) AS unique_partners
+FROM daily_sales
+GROUP BY date_id, make_name
